1c2e31bb8d41561929cfdcf23833392772648656
Gamir <gamir07@yandex.ru>
2024-09-08 12:37:16 +0300

    первый коммит

---
+++ src/test/java/ru/ggainullin/recomendationservice/RecomendationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ggainullin.recomendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
